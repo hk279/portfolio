7e44d8b4c5529d1915cba39835d8b44367d3995e
Jere Järvinen <jerejarvinen94@gmail.com>
2020-01-08 18:03:46 +0200

    Added an email link

--- src/components/CV_sections/Contact.js
+++ src/components/CV_sections/Contact.js
@@ -8,8 +8,7 @@ const Contact = () => {
         <div className="fade-in">
             <h1>Contact Info</h1>
             <p>Feel free to contact me by email for my full detailed resume and contact information.</p>
-            <h3>Email</h3>
-            <p>jerejarvinen94@gmail.com</p>
+            <a id="email-link" href="mailto:jerejarvinen94@gmail.com?">jerejarvinen94@gmail.com</a><br/>
             <div style={{cursor: "pointer", display: "inline-block"}} onClick={() => window.location.href='https://www.linkedin.com/in/jere-j%C3%A4rvinen-b0b64b89/'}>
                 <FontAwesomeIcon icon={faLinkedin} size="5x" />
             </div>
