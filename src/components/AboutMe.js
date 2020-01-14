47e32f302458faf6e9a7252387c4f50906c75bb2
Jere Järvinen <jerejarvinen94@gmail.com>
2020-01-14 22:33:44 +0200

    Fixed typo

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -7,7 +7,7 @@ const MyStory = () => {
             <div className="content" style={{textAlign: "left"}}>
                 <h3>About Me</h3>
                 <p>
-                    My story as a developer stated after I graduated as a BBA. For a while, I worked in B2B-sales for a software company for awhile and my time there inspired me to start pursuing a career in the development side of things.
+                    My story as a developer stated after I graduated as a BBA. For a while, I worked in B2B-sales for a software company and my time there inspired me to start pursuing a career in the development side of things.
                 </p>
                 <p>
                     I moved to Espoo in order to study business IT and now I feel like I've found my calling. I enjoy the ever-changing nature of the field and especially the feeling of solving problems and making things work.
