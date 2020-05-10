97a6337dedb6db25dd7d3d23ae8dc3d40d3816e7
Jere Järvinen <jerejarvinen94@gmail.com>
2020-05-10 18:36:30 +0300

    Added more content

--- src/components/CV_sections/Expertise.js
+++ src/components/CV_sections/Expertise.js
@@ -1,34 +1,58 @@
-import React from 'react'
-import '../../App.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faJs, faReact, faPhp, faJava } from '@fortawesome/free-brands-svg-icons'
+import React from "react";
+import "../../App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faJs, faReact, faNode, faJava } from "@fortawesome/free-brands-svg-icons";
+import { faDatabase } from "@fortawesome/free-solid-svg-icons";
 
 const Expertise = () => {
-    return(
+    return (
         <div className="fade-in">
             <h1>Expertise</h1>
-            <table style={{width: "80%", margin: "auto"}}>
+            <table style={{ width: "80%", margin: "auto" }}>
                 <tbody>
                     <tr>
-                        <td><FontAwesomeIcon icon={faJs} size="3x" /></td>
-                        <td>My strongest area of expertise is JavaScript and most of my projects are made with it.</td>
+                        <td>
+                            <FontAwesomeIcon icon={faJs} size="3x" />
+                        </td>
+                        <td>
+                            I have been working with vanilla Javascript for several years and possess a good knowledge
+                            of its core features.
+                        </td>
                     </tr>
                     <tr>
-                        <td><FontAwesomeIcon icon={faReact} size="3x" /></td>
-                        <td>After getting a good understanding of vanilla JS, I have gotten more into React. This website is also made with React.</td>
+                        <td>
+                            <FontAwesomeIcon icon={faReact} size="3x" />
+                        </td>
+                        <td>
+                            After getting a good understanding of vanilla JS, I have gotten more into React. Front-end
+                            in my most recent projects is build with React.
+                        </td>
                     </tr>
                     <tr>
-                        <td><FontAwesomeIcon icon={faPhp} size="3x" /></td>
-                        <td>I haven't worked with PHP as much as JS, but one of my portfolio projects (Quiz App) is made with it.</td>
+                        <td>
+                            <FontAwesomeIcon icon={faNode} size="3x" />
+                        </td>
+                        <td>
+                            Node.js is my most used backend technology. Express.js and templating (EJS) are also part of
+                            my expertise.
+                        </td>
                     </tr>
                     <tr>
-                        <td><FontAwesomeIcon icon={faJava} size="3x" /></td>
+                        <td>
+                            <FontAwesomeIcon icon={faDatabase} size="3x" />
+                        </td>
+                        <td>In my previous projects I have used MySQL and MongoDB database solutions.</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <FontAwesomeIcon icon={faJava} size="3x" />
+                        </td>
                         <td>Basic knowledge of Java. I haven't created any serious applications with it.</td>
                     </tr>
                 </tbody>
             </table>
         </div>
-    )
-}
+    );
+};
 
-export default Expertise
+export default Expertise;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,55 +1,65 @@
-import React from 'react'
-import '../App.css'
-import ProjectCard from './ProjectCard'
-import quizApp from '../images/quiz_app.PNG'
-import todoList from '../images/todo_list.png'
-import finnkinoSearch from '../images/finnkino_search.PNG'
-import mealRecipes from '../images/meal_recipes.png'
+import React from "react";
+import "../App.css";
+import ProjectCard from "./ProjectCard";
+import quizApp from "../images/quiz_app.PNG";
+import videoGameSales from "../images/video_game_sales.PNG";
+import finnkinoSearch from "../images/finnkino_search.PNG";
+import mealRecipes from "../images/meal_recipes.png";
 
 class Projects extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
             projectCards: [
+                {
+                    title: "Videogame Sales CRUD app",
+                    description:
+                        "A full-stack app using a Node.js / Express.js server with an API. Front-end is made built with React using Shards UI toolkit.",
+                    image: videoGameSales,
+                    url: "https://github.com/hk279/Project-3-Frontend",
+                },
+                {
+                    title: "Meal Recipes",
+                    description:
+                        "A React application that allows the user to browse and search for recipes in the Meal DB -API. UI created with React Bootstrap.",
+                    image: mealRecipes,
+                    url: "https://github.com/hk279/MealRecipes",
+                },
                 {
                     title: "Finnkino Lite Search",
                     description: "Light search utility for movies in Finnkino theatres.",
                     image: finnkinoSearch,
-                    url: "https://github.com/hk279/movies_webapp"
+                    url: "https://github.com/hk279/movies_webapp",
                 },
                 {
                     title: "Quiz App",
-                    description: "PHP web app for teachers to create quizzes for students. Connected to a MySQL database.",
+                    description: `A PHP web app for teachers to create quizzes and small exams. 
+                        Separate admin and student logins to manage or answer quizzes respectively. 
+                        Connected to a MySQL database.`,
                     image: quizApp,
-                    url: "https://github.com/hk279/quiz_app"
+                    url: "https://github.com/hk279/quiz_app",
                 },
-                {
-                    title: "Todo List",
-                    description: "A web app for saving items containing a title, a type, a description and a due date. Uses local storage.",
-                    image: todoList,
-                    url: "https://github.com/hk279/todo_list_app"
-                },
-                {
-                    title: "Meal Recipes",
-                    description: "A React application that allows the user to browse and search for recipes in the Meal DB -API.",
-                    image: mealRecipes,
-                    url: "https://github.com/hk279/MealRecipes"
-                }
-            ]
-        }
+            ],
+        };
     }
     render() {
-        return(
+        return (
             <section className="projects background">
                 <div className="content">
                     <h2>Projects</h2>
-                    {this.state.projectCards.map((item, i) => 
-                        <ProjectCard key={item.title} title={item.title} description={item.description} image={item.image} url={item.url}/>
-                    )}
+                    {this.state.projectCards.map((item) => (
+                        <ProjectCard
+                            key={item.title}
+                            title={item.title}
+                            description={item.description}
+                            image={item.image}
+                            url={item.url}
+                        />
+                    ))}
                 </div>
             </section>
-        )
+        );
     }
 }
 
-export default Projects
+export default Projects;
