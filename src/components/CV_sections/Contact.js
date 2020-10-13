58f4aa3b5c27608c8e3984025b12df33e9698070
Jere Järvinen <AA5536@student.jamk.fi>
2020-10-13 19:22:36 +0300

    edited contact section of the CV

--- src/components/CV_sections/Contact.js
+++ src/components/CV_sections/Contact.js
@@ -1,19 +1,26 @@
-import React from 'react'
-import '../../App.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import React from "react";
+import "../../App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const Contact = () => {
-    return(
+    return (
         <div className="fade-in">
             <h1>Contact Info</h1>
             <p>Feel free to contact me by email for my full detailed resume and contact information.</p>
-            <a id="email-link" href="mailto:jerejarvinen94@gmail.com?">jerejarvinen94@gmail.com</a><br/>
-            <div style={{cursor: "pointer", display: "inline-block"}} onClick={() => window.location.href='https://www.linkedin.com/in/jere-j%C3%A4rvinen-b0b64b89/'}>
+            <br />
+            <div className="contact-icon" onClick={() => (window.location.href = "mailto:jerejarvinen94@gmail.com")}>
+                <FontAwesomeIcon icon={faEnvelope} size="5x" />
+            </div>
+            <div
+                className="contact-icon"
+                onClick={() => (window.location.href = "https://www.linkedin.com/in/jere-j%C3%A4rvinen-b0b64b89/")}
+            >
                 <FontAwesomeIcon icon={faLinkedin} size="5x" />
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Contact
+export default Contact;
