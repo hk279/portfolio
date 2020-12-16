64dece0ade85e3f57c1038f7d0b3c687ccb08a33
Jere Järvinen <AA5536@student.jamk.fi>
2020-12-16 21:09:53 +0200

    Updates

---
+++ precache-manifest.b1d6c4f2d15f7f8e4044e4758f053cad.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25447e30e415ded3b5b0817a8184b502",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4e4c49c622a76d1032c2",
+    "url": "/portfolio/static/css/2.ff29e5f6.chunk.css"
+  },
+  {
+    "revision": "2de7eb110e25ce0e70d5",
+    "url": "/portfolio/static/css/main.8e4ac0a0.chunk.css"
+  },
+  {
+    "revision": "4e4c49c622a76d1032c2",
+    "url": "/portfolio/static/js/2.fedba3f2.chunk.js"
+  },
+  {
+    "revision": "2de7eb110e25ce0e70d5",
+    "url": "/portfolio/static/js/main.7f0fc29e.chunk.js"
+  },
+  {
+    "revision": "b74baeb4ef4604f0eca7",
+    "url": "/portfolio/static/js/runtime-main.11d313f3.js"
+  },
+  {
+    "revision": "ed4935f324a7c15306082bd872cddf66",
+    "url": "/portfolio/static/media/digital_background01.ed4935f3.jpg"
+  },
+  {
+    "revision": "d0b85bd664b30ad101312058ac2d359e",
+    "url": "/portfolio/static/media/digital_background02.d0b85bd6.png"
+  },
+  {
+    "revision": "5c6bdad49bc4216a143668b0834c6b69",
+    "url": "/portfolio/static/media/digital_background03.5c6bdad4.jpg"
+  },
+  {
+    "revision": "318f8638e996472528e38e3dc8a16bb7",
+    "url": "/portfolio/static/media/digital_background04.318f8638.jpg"
+  },
+  {
+    "revision": "be9510ddd1192dba515dafbadff99d61",
+    "url": "/portfolio/static/media/meal_recipes.be9510dd.png"
+  },
+  {
+    "revision": "ca1bab250342bd5280a5d8cc410e832c",
+    "url": "/portfolio/static/media/movies_search_login.ca1bab25.png"
+  },
+  {
+    "revision": "ad965bb542e2acb37772b33dfe40fe97",
+    "url": "/portfolio/static/media/photo.ad965bb5.jpg"
+  },
+  {
+    "revision": "7cfa3619f47845a5fd4ba392e96c8d9c",
+    "url": "/portfolio/static/media/quiz_app.7cfa3619.PNG"
+  },
+  {
+    "revision": "7c5a66425d550e322217184c0acc3c4b",
+    "url": "/portfolio/static/media/react-native.7c5a6642.jpeg"
+  },
+  {
+    "revision": "3347844aa9f78a790a5791d9f6f230e6",
+    "url": "/portfolio/static/media/video_game_sales.3347844a.PNG"
+  },
+  {
+    "revision": "1f76f416de12eae07132d4b83f088ef2",
+    "url": "/portfolio/static/media/warehouse_assistant.1f76f416.PNG"
+  }
+]);
